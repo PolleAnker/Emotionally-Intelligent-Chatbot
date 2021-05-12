5b967e970e6d018a05ce5105ccb6188a787c759a
PolleAnker <thementoryay@gmail.com>
2021-05-12 16:42:03 +0200

    Can now set name, and RoboFriend says hi!

--- Assets/RoboFriendManager.cs
+++ Assets/RoboFriendManager.cs
@@ -1,22 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class RoboFriendManager : MonoBehaviour
 {
-    public string userName;
-    
-    public GameObject nameInput;
+    private string inputMessage;
+    private string userName;
 
-    // Start is called before the first frame update
+    public GameObject enterNameInterface;
+    public GameObject interactionInterface;
+
+    public GameObject roboFriendMessageField;
+    private TextMeshProUGUI roboFriendSays;
+
+    private bool helloName = false;
+
     void Start()
     {
-        
-    }
+        roboFriendSays = roboFriendMessageField.GetComponent<TextMeshProUGUI>();
+    }  
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!string.IsNullOrEmpty(userName) && !helloName)
+        {
+            roboFriendSays.text = "Hello " + userName + "! How are you today?";
+            helloName = true;
+        }
+
+    }
+
+    public void SetUserName(string s)
+    {
+        userName = s;
+        Debug.Log(userName);
+    }
+
+    public void ChangeToInteraction()
+    {
+        enterNameInterface.SetActive(false);
+        interactionInterface.SetActive(true);
     }
 }
