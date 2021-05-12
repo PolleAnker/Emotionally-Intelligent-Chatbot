009940ee838022cd1e596011749fa5c59ca8bd2e
PolleAnker <thementoryay@gmail.com>
2021-05-12 17:12:33 +0200

    Expressions now change on input
    more interaction needs to be made

--- Assets/RoboFriendManager.cs
+++ Assets/RoboFriendManager.cs
@@ -8,6 +8,10 @@ public class RoboFriendManager : MonoBehaviour
 {
     private string inputMessage;
     private string userName;
+    private string msgToRoboFriend;
+
+    public GameObject currentFace;
+    public GameObject[] faces;
 
     public GameObject enterNameInterface;
     public GameObject interactionInterface;
@@ -15,11 +19,16 @@ public class RoboFriendManager : MonoBehaviour
     public GameObject roboFriendMessageField;
     private TextMeshProUGUI roboFriendSays;
 
+    public GameObject userSaidField;
+    private TextMeshProUGUI userSaid;
+
     private bool helloName = false;
 
     void Start()
     {
         roboFriendSays = roboFriendMessageField.GetComponent<TextMeshProUGUI>();
+        userSaid = userSaidField.GetComponent<TextMeshProUGUI>();
+        currentFace = faces[0];
     }  
 
     // Update is called once per frame
@@ -31,17 +40,47 @@ public class RoboFriendManager : MonoBehaviour
             helloName = true;
         }
 
+        if(!string.IsNullOrEmpty(msgToRoboFriend))
+        {
+            if(msgToRoboFriend.Contains("good"))
+            {
+                currentFace.SetActive(false);
+                faces[2].SetActive(true);
+                currentFace = faces[2];
+                roboFriendSays.text = "I'm so happy to hear that " + userName + "! Why are you good?";
+            }
+            if(msgToRoboFriend.Contains("bad"))
+            {
+                roboFriendSays.text = "I'm sorry to hear that " + userName + "! What's wrong?";
+                currentFace.SetActive(false);
+                faces[1].SetActive(true);
+                currentFace = faces[1];
+            }
+            else
+            {
+                roboFriendSays.text = "I don't understand what you are saying " + userName + ", sorry!";
+                currentFace.SetActive(false);
+                faces[1].SetActive(true);
+                currentFace = faces[1];
+            }
+        }
+
     }
 
     public void SetUserName(string s)
     {
         userName = s;
-        Debug.Log(userName);
     }
 
     public void ChangeToInteraction()
     {
         enterNameInterface.SetActive(false);
         interactionInterface.SetActive(true);
+    }
+
+    public void TellRoboFriend(string s)
+    {
+        msgToRoboFriend = s;
+        userSaid.text = msgToRoboFriend;
     }
 }
