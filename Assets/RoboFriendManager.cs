e37993324f2bd5e4f7cbbd4651599aaa54654994
PolleAnker <thementoryay@gmail.com>
2021-05-12 15:02:32 +0200

    UI addition with buttons and text fields

---
+++ Assets/RoboFriendManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoboFriendManager : MonoBehaviour
+{
+    public string userName;
+    
+    public GameObject nameInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
